4c9675258052d09cfd85a07997afb6740696a4c2
mrauzi <mike.rauzi@gmail.com>
2016-11-22 11:07:00 -0600

    Add new User Repository data in main program.

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,12 +13,23 @@ namespace ConsoleSample
         static void Main(string[] args)
         {
 
-            EntityRepository entityRepository = new EntityRepository();
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
+
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetByID(Guid.NewGuid());
+
+            myUser.Name = "Tom";
+            myUser.EmailAddress = "Tom@email.com";
+            Console.WriteLine("New entity ID: " + myUser.ID.ToString());
+            Console.WriteLine("New entity Name: " + myUser.Name);
+            Console.WriteLine("New entity email address: " + myUser.EmailAddress);
+
+            Console.ReadKey();
 
         }
     }
